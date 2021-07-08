3dc45ecafcbd978dc48ba02abe54a32fc094df1e
Kane <kgin888@gmail.com>
2021-07-08 16:54:15 +0100

    wrote FT for cant enter blank item

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,7 +8,28 @@ class ItemValidationTest(FunctionalTest):
     self.browser.get(self.live_server_url)
 
     inputbox = self.browser.find_element_by_id('id_new_item')
-    inputbox.send_keys('')
-    inputbox.send_keys(Keys.ENTER)   
+    inputbox.send_keys(Keys.ENTER)  
 
-    self.fail('write me') 
+    self.wait_for(lambda: self.assertEqual(
+      self.browser.find_element_by_css_selector('.has-error').text,
+      "You can't have an empty list item"
+    ))
+
+    inputbox = self.browser.find_elemebt_by_id('id_new_item')
+    inputbox.send_keys('Buy milk')
+    inputbox.send_keys(Keys.ENTER)  
+    self.wait_for_row_in_list_table('1: Buy milk')
+
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    inputbox.send_keys(Keys.ENTER)  
+    
+    self.wait_for(lambda: self.assertEqual(
+      self.browser.find_element_by_css_selector('.has-error').text,
+      "You can't have an empty list item"
+    ))
+
+    inputbox = self.browser.find_elemebt_by_id('id_new_item')
+    inputbox.send_keys('Make tea')
+    inputbox.send_keys(Keys.ENTER)  
+    self.wait_for_row_in_list_table('1: Buy milk')
+    self.wait_for_row_in_list_table('2: Make tea')
