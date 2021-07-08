50fbe9daca19b62c13c271d868e952a1e65014c2
Kane <kgin888@gmail.com>
2021-07-08 13:11:48 +0100

    heroku deploy

--- superlists/settings.py
+++ superlists/settings.py
@@ -132,4 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
+ALLOWED_HOSTS = ['kanes-notes.herokuapp.com']
+
 django_heroku.settings(locals())
