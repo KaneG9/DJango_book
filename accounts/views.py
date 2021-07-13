af11381ed34f80f727b8e21ca285864aa9ba4a41
Kane <kgin888@gmail.com>
2021-07-13 16:31:06 +0100

    created login email

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.core.mail import send_mail
+from django.contrib import messages
+from accounts.models import Token
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+def send_login_email(request):
+  email = request.POST['email']
+  token = Token.objects.create(email = email)
+  url = request.build_absolute_uri(
+    reverse('login') + '?token=' + str(token.uid)
+  )
+  send_mail('Your login link for Superlists', f'Use this link to login:\n\n{url}', 'kgin52@gmail.com', [email])
+  messages.success(
+    request,
+    "Check your email, we've sent you a link you can use to log in."
+  )
+  return redirect('/')
+
+def login(request):
+  return redirect('/')
